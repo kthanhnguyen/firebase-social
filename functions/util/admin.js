3bc5c7d329a388d37a16d7099fef964e5c2492c8
kthanhnguyen <nguyenkimthanh901@gmail.com>
2020-06-23 22:00:42 +0700

    update

--- functions/util/admin.js
+++ functions/util/admin.js
@@ -1,10 +1,18 @@
 const admin = require("firebase-admin");
 var serviceAccount = require("../serviceAccountKey.json");
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+const firebaseConfig = {
+  apiKey: "AIzaSyASTX9pxYCRq4PoBWC-pOBFpmkzhSkcUKQ",
+  authDomain: "socialapp-32ed8.firebaseapp.com",
   databaseURL: "https://socialapp-32ed8.firebaseio.com",
-});
+  projectId: "socialapp-32ed8",
+  storageBucket: "socialapp-32ed8.appspot.com",
+  messagingSenderId: "904882535502",
+  appId: "1:904882535502:web:9a9493978e7b18e8f7171f",
+  measurementId: "G-621QNVH4Q0",
+};
+
+admin.initializeApp(firebaseConfig);
 
 const db = admin.firestore();
 
